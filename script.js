d1cd932f6b4afe4696161fe8b3fa5282262ef4d2
Jose Del Valle <jdelvalle88@live.com>
2022-11-23 18:41:26 -0500

    codes onto javascript

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 // Assignment code here
-var alphaLowerCase = "abcdefghijklmnopqrstuvwxyz";
-var alphaUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = 0123456789;
-var symbols = "!@#$%^&*()";
-var passwordLength = 8;
+var characters = "abcdefghijklmnopqrstuvwxyz, ABCDEFGHIJKLMNOPQRSTUVWXYZ, 0123456789, @#$%^&*()";
+var passwordLength = 12;
 var password = " ";
-
+console.log(characters);
 // Get references to the #generate element
-var generateBtn = document.querySelector("generate");
-var passwordChoice = window.prompt("Enter 8 characters, symbol, one uppercase, lowercase & numeric");
+var generateBtn = document.querySelector("#generate");
+var passwordChoice = window.prompt("Enter 12 characters, symbol, one uppercase, lowercase & numeric");
 // Write password to the #password input
 function writePassword() {
 
-  let password = generatePassword();
-  let passwordText = document.querySelector("password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  for(var i = 0 ; i < passwordLength ; i++) {
-    if(alpha) {
-      generatePassword +
-    }
-  }
   passwordText.value = password;
 
 }
@@ -27,5 +19,23 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-  let passwordText.value 
-  result.value = generatePassword (alpha, numbers, symbols);
+ function generatePassword () {
+    var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+    var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var numbers = '0123456789';
+    var symbols = '@#$%^&*()';
+ 
+for(var i = 0 ; i < passwordLength ; i++);
+
+if (characters === 'abcdefghijklmnopqrstuvwxyz') {
+    console.log("lowercase");
+   } else if (characters === 'ABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+    console.log("uppercase");
+   } else if (characters === '0123456789') {
+    console.log("numbers");
+   } else if (characters === '@#$%^&*()') {
+    console.log("symbols");
+   } else {
+    console.log('Does not meet the criteria!');
+   }
+ }
