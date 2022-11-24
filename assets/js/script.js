1b14a340d04f97e39f3bb768c4b3df862ac85805
jdelvalle12 <104731082+jdelvalle12@users.noreply.github.com>
2022-11-23 22:29:30 -0500

    new codes added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,40 +2,48 @@
 var characters = "abcdefghijklmnopqrstuvwxyz, ABCDEFGHIJKLMNOPQRSTUVWXYZ, 0123456789, @#$%^&*()";
 var passwordLength = 12;
 var password = " ";
-console.log(characters);
+console.log(characters, passwordLength, password);
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var passwordChoice = window.confirm("Enter 12 characters, symbol, one uppercase, lowercase & numeric");
+var passwordText = document.querySelector("#password");
+
+var passwordChoice = window.alert("Enter at least 12 characters, a symbol, one uppercase, lowercase & number");
+var index = Math.floor(Math.random() * passwordLength);
+var computerChoice = characters[index];
 // Write password to the #password input
 function writePassword() {
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  document.querySelector("#password").textContent = password;
+  var password = generatePassword(generate, password);
+  
 
   passwordText.value = password;
-  var passwordChoice = window.confirm("Enter 12 characters, symbol, one uppercase, lowercase & numeric");
+  
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword () {
+  var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbers = '0123456789';
+  var symbols = '@#$%^&*()';
 
- function generatePassword () {
-    var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-    var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var numbers = '0123456789';
-    var symbols = '@#$%^&*()';
- 
 for(var i = 0 ; i < passwordLength ; i++);
 
-if (characters === 'abcdefghijklmnopqrstuvwxyz') {
-    console.log("lowercase");
-   } else if (characters === 'ABCDEFGHIJKLMNOPQRSTUVWXYZ') {
-    console.log("uppercase");
-   } else if (characters === '0123456789') {
-    console.log("numbers");
-   } else if (characters === '@#$%^&*()') {
-    console.log("symbols");
-   } else {
-    console.log('Does not meet the criteria!');
-   }
+if (characters ==='abcdefghijklmnopqrstuvwxyz, ABCDEFGHIJKLMNOPQRSTUVWXYZ, 0123456789, @#$%^&*()' && passwordChoice === 'abcdefghijklmnopqrstuvwxyz, ABCDEFGHIJKLMNOPQRSTUVWXYZ, 0123456789, @#$%^&*()') {
+  windows.alert('Password Accepted!');
+  console.log("Password accepted!");
+} else if (characters === 'abcdefghijklmnopqrstuvwxyz') {
+  console.log("lowercase");
+ } else if (characters === 'ABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+  console.log("uppercase");
+ } else if (characters === '0123456789') {
+  console.log("numbers");
+ } else if (characters === '@#$%^&*()') {
+  console.log("symbols");
+ } else {
+  console.log('Does not meet the criteria!');
  }
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+passwordText.addEventListener("click", generatePassword);
