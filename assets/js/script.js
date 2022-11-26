2b952fc305742abcc575788c6a181f24208ad816
jdelvalle12 <104731082+jdelvalle12@users.noreply.github.com>
2022-11-26 15:40:34 -0500

    new codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,34 +15,65 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  password = generatePassword();
+  document.querySelector("password").textContent = password;
+//Function to generate password
+//Variables to input:
+var passwordChoice;
+var confirmNumber;
+var confirmSymbols;
+var confirmUppercase;
+var confirmLowercase;
 
-//Functions to generate password;
-
-var lowerCase = ["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//Password variable values;
+var options = ['alpha', 'symbols', 'numbers']
+//Alpha
+var alpha = ["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//Symbols
 var symbols = ["@", "#", "$", "%", "^", "&", "*", "(", ")"];
+//Numbers
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var passwordLength = 128;
+//Max-length of the password
+var length = 128;
 var password = " ";
+
+//Convert to uppercase 
+space = [];
+//Choices declared outside the if statement so they can be concatenated upon condition var choice;
+//Converts letters to uppercase
+  var toUppercase = function (x) {
+    return x.toUpperCase();
+  }
+
+//creates a variable for uppercase conversion
+alpha2 = alpha.map(toUpper);
 
 function generatePassword() {
 
-  var generateBtn = parseInt(alert("Enter 8-128 characters, at least one symbol, one uppercase, one number, & lowercase"));
-  var passwordChoice = ['lowerCase', 'upperCase', 'numbers', 'symbols', 'passwordLength'];
+  var passwordChoice = parseInt(alert("Enter 8-128 characters, at least one symbol, one uppercase, one number, & lowercase"));
+
+  var index = Math.floor(Math.random() * 8);
+  var passwordChoice = options[index];
   
 
 for(var i = 0 ; i < passwordLength ; i++);
-(passwordChoice < 8 || passwordChoice > 128);
 
 if (!passwordChoice) {
-  alert("This needs value");
-} else if {
-  (passwordChoice ==='lowerCase','upperCase', 'numbers', 'symbols');  
-} else if {
-  (passwordChoice < 8 || passwordChoice > 128) {
-    passwordChoice = parseInt(prompt('choose between 8 to 128 characters'));
-  }
+  alert("This needs a value");
+  return;
+} else if (passwordChoice < 8 || passwordChoice > 128) {
+  passwordChoice = parseInt(prompt("You must choose between 8 and 128"));
+} else {
+  confirmNumber = confirm("Will this containt numbers?");
+  confirmSymbols = confirm("Will this contain symbols?");
+  confirmUppercase = confirm("Will this contain Uppercase letters?");
+  confirmLowercase = confirm("Will this contain Lowercase letters?");
+};
+
+//Else if 4 negative options
+if(!confirmSymbols && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+  choices = alert("You must choose a criteria!");
+}
   console.log('Does not meet the criteria!');
- }
 }
 generatePassword()
